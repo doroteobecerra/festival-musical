35c33dd2a5c9d3fe67b5357929bfc407ca1fa162
doroteobecerra <doroteobecerra@outlook.com>
2022-09-15 22:40:58 -0500

    se agregaros los estilos globales en scsss

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const {src, dest, watch} = require("gulp")
 const sass = require('gulp-sass')(require('sass'))
+const plumber = require('gulp-plumber');
 
 function css(done){
     src('src/scss/app.scss')//indentifica el archivo
+        .pipe(plumber())
         .pipe(sass())//compila
         .pipe(dest('build/css'))//guarda el resultado en un file css
 
@@ -10,10 +12,10 @@ function css(done){
 }
 
 function dev(done){
-    watch('src/scss/app.scss', css)
+    watch('src/scss/**/*.scss', css)
 
     done()
 }
 
 exports.css = css
-exports.dev = dev
+exports.dev = dev  
