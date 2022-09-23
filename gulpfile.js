c540fb83cc3ab280f2d8373e64d1090e56c97085
doroteobecerra <doroteobecerra@outlook.com>
2022-09-22 20:32:22 -0500

    se utilizo gulp para reducir el tamaño de las imagenes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
-const {src, dest, watch} = require("gulp")
-const sass = require('gulp-sass')(require('sass'))
+const {src, dest, watch , parallel} = require("gulp");
+//css
+const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+// imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done){
     src('src/scss/app.scss')//indentifica el archivo
@@ -11,6 +17,42 @@ function css(done){
     done()
 }
 
+function imagenes(done){
+    const opciones = {
+        optimizationLevel: 3
+
+    }
+
+    src('img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'))
+    done()
+}
+
+function versionWebp(done){
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+    done();
+}
+
+function versionAvif(done){
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'))
+    done();
+}
+
 function dev(done){
     watch('src/scss/**/*.scss', css)
 
@@ -18,4 +60,7 @@ function dev(done){
 }
 
 exports.css = css
-exports.dev = dev  
+exports.imagenes = imagenes
+exports.versionWebp = versionWebp
+exports.versionAvif = versionAvif
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev)
